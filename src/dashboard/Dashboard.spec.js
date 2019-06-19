9e33ec783642b6c63de143876bfe52274078eefb
PetejStone <petejstoney@gmail.com>
2019-06-19 13:08:06 -0700

    added first test for checking default status of gate

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+// Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+//import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+
+import Dashboard from './Dashboard';
+
+
+describe('<Display>', () => {
+    it('runs test', () => {
+        expect(true).toBe(true)
+    })
+
+
+  it('matches snapshot', () => {
+      const { snapshot } = render(<Dashboard />)
+      expect(snapshot).toMatchSnapshot()
+  })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+//import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+
+import Display from './Display';
+
+
+
+
+
+//checks default Gate value
+describe('<Display', () => {
+    describe('Display content', () => {
+        it('renders unlocked and open', () => {
+            const { getByText } = render(<Display />)
+            getByText(/unlocked/i)
+            getByText(/open/i)
+        })
+    })
+    
+})
