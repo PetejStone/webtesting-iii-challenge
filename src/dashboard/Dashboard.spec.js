4f3cdde67da709eb5c3591e78e3262a67f6c45c9
PetejStone <petejstoney@gmail.com>
2019-06-19 14:23:27 -0700

    added test for open gate button

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -23,47 +23,94 @@ describe('<Dashboard>', () => {
     describe('button behavior', () => {
         it('toggle locked and open status for CLOSE GATE', () => {
             const { getByText, getAllByText, queryByText } = render(<Dashboard />)
+             //find lock gate button and close gate
+             const lockGate = getByText(/^lock gate$/i)
+             const close = getByText(/^close gate$/i)
             
-            //find lock gate button
-            const lockGate = getByText(/^lock gate$/i)
+    
 
-            //click button
+            //click lock gate (deactivatd)
             fireEvent.click(lockGate)
 
-            //what happens when lock gate is clicked -- (nothing changes)
+            //lockgate button should be deativated
             expect(getByText(/open/i)).toBeTruthy
             expect(getByText(/lock gate/i)).toBeTruthy
             expect(getByText(/close gate/i)).toBeTruthy
 
-
-            
-
-            //find close button
-            const close = getByText(/^close gate$/i)
-
-            //click button
+            //click close
             fireEvent.click(close)
-
 
             //what happens when close is clicked
             expect(getByText(/closed/i)).toBeTruthy
             expect(getByText(/lock gate/i)).toBeTruthy
             expect(getByText(/open gate/i)).toBeTruthy
+        })
 
+        it('should test LOCK GATE button', () => { 
+            const { getByText, getAllByText, queryByText } = render(<Dashboard />)
+            const lockGate = getByText(/^lock gate$/i)
+            const close = getByText(/^close gate$/i)
             //lock gate should now be deactivated
              
 
             //click lock gate button should now toggle closed/open and locked/unlocked
+            fireEvent.click(close)
             fireEvent.click(lockGate)
-            
+
 
              //what happens when lock gate is clicked -- toggle per above
              expect(getByText(/closed/i)).toBeTruthy
              expect(getByText(/unlock gate/i)).toBeTruthy
              expect(getByText(/open gate/i)).toBeTruthy
              expect(getByText(/locked/i)).toBeTruthy
+            })
+
+        it('should test OPEN GATE button', () => {
+            const { getByText, getAllByText, queryByText } = render(<Dashboard />)
+            const close = getByText(/^close gate$/i)
+           
+
+          
+            
+
+            //click close gate gate
+            fireEvent.click(close)
+
+            //open gate now available
+            const openGate = getByText(/^open gate$/i)
+           
+            //click open gate
+            fireEvent.click(openGate)
+
+            //nothing should change as it is deactivated
+            expect(getByText(/open/i)).toBeTruthy
+            expect(getByText(/close gate/i)).toBeTruthy
+            expect(getByText(/lock gate/i)).toBeTruthy
+            expect(getByText(/^unlocked$/i)).toBeTruthy
+
 
         })
+
+        // it('should test unlock gate button', () => {
+        //     const { getByText, getAllByText, queryByText } = render(<Dashboard />)
+        
+        //      //find unlock gate button
+        //     const unlockGate = getByText(/^unlock gate$/i)
+
+        //     //click button
+        //     fireEvent.click(unlockGate)
+
+        //     //what happens when unlock gate is clicked -- reverse locked and make open gate available
+        //     expect(getByText(/closed/i)).toBeTruthy
+        //     expect(getByText(/lock gate/i)).toBeTruthy
+        //     expect(getByText(/open gate/i)).toBeTruthy
+        //     expect(getByText(/locked/i)).toBeTruthy
+
+        // })
+
+         
+        
+        
     })
     
 })
